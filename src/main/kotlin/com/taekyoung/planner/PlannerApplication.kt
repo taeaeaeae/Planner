17711decec091968bbfdc531cf5b0f4efca7d6ad
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-06-20 14:40:01 +0900

    Initial commit

---
+++ src/main/kotlin/com/taekyoung/planner/PlannerApplication.kt
@@ -0,0 +1,11 @@
+package com.taekyoung.planner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlannerApplication
+
+fun main(args: Array<String>) {
+    runApplication<PlannerApplication>(*args)
+}
---
+++ src/test/kotlin/com/taekyoung/planner/PlannerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.taekyoung.planner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
