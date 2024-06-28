f4c040124ceeb5a048f1ce59cea0b77fff05cc5d
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-06-28 17:31:40 +0900

    :sparkles: feat: 쿼리디에스엘

---
+++ src/main/kotlin/com/taekyoung/planner/domain/common/QueryDslConfig.kt
@@ -0,0 +1,15 @@
+package com.taekyoung.planner.domain.common
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import jakarta.persistence.EntityManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class QueryDslConfig {
+
+    @Bean
+    fun jpaQueryFactory(entityManager: EntityManager): JPAQueryFactory {
+        return JPAQueryFactory(entityManager)
+    }
+}
