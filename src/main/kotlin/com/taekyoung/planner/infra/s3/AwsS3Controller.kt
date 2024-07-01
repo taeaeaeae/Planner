d426dbccebe274efbf3ab2bee7f4dc4f60729c7f
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-07-01 16:44:27 +0900

    :truck: rename: class 이름 변경

--- src/main/kotlin/com/taekyoung/planner/infra/s3/AwsS3Controller.kt
+++ src/main/kotlin/com/taekyoung/planner/infra/s3/AwsS3Controller.kt
@@ -2,14 +2,13 @@ package com.taekyoung.planner.infra.s3
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
-class awsS3(
+class AwsS3Controller(
     private val service: FileUploadService
 ) {
 
